595d4610492874094516961abc60af4030224784
Ray Villalobos <ray@lynda.com>
2014-06-23 16:10:35 -0400

    02_04 Binding Data

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-d3.selectAll('.item:nth-child(3)')
+var myStyles = [
+  { width: 200,
+    color: '#A57706'},
+  { width: 230,
+    color: '#BD3613'},
+  { width: 220,
+    color: '#D11C24'},
+  { width: 290,
+    color: '#C61C6F'},
+  { width: 236,
+    color: '#595AB7'},
+  { width: 230,
+    color: '#2176C7'}
+];
+
+d3.selectAll('.item')
+  .data(myStyles)
   .style({
-    'background': '#268BD2',
-    'padding': '10px',
-    'margin' : '5px',
-    'color' : '#EEE8D5'
+    'color': 'white',
+    'background' : function(d) {
+      return d.color;
+    },
+    width : function(d) {
+      return d.width + 'px';
+    } 
   })
