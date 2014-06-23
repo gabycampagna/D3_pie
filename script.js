85662575a97be96503068f22e5c7ab8fa22fec27
Ray Villalobos <ray@lynda.com>
2014-06-23 15:35:34 -0400

    02_03 Modifying Attributes

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-d3.select('#chart .item:nth-child(3)')
-  .remove()
+d3.selectAll('.item:nth-child(3)')
+  .style({
+    'background': '#268BD2',
+    'padding': '10px',
+    'margin' : '5px',
+    'color' : '#EEE8D5'
+  })
