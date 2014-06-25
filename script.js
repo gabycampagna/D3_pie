b63b9ac2c13a5f07c7783a81cc6a6851e993fcac
Ray Villalobos <ray@lynda.com>
2014-06-24 21:53:07 -0400

    02_04 Ordinal Scales

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var bardata = [20, 30, 20, 15, 40, 200];
+var bardata = [20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80];
 
 var height = 400,
     width = 600,
@@ -7,7 +7,11 @@ var height = 400,
 
 var yScale = d3.scale.linear()
         .domain([0, d3.max(bardata)])
-        .range([0, height])
+        .range([0, height]);
+
+var xScale = d3.scale.ordinal()
+        .domain(d3.range(0, bardata.length))
+        .rangeBands([0, width])
 
 d3.select('#chart').append('svg')
     .attr('width', width)
@@ -16,12 +20,12 @@ d3.select('#chart').append('svg')
     .selectAll('rect').data(bardata)
     .enter().append('rect')
         .style('fill', '#C61C6F')
-        .attr('width', barWidth)
+        .attr('width', xScale.rangeBand())
         .attr('height', function(d) {
             return yScale(d);
         })
         .attr('x', function(d,i) {
-            return i * (barWidth + barOffset);
+            return xScale(i);
         })
         .attr('y', function(d) {
             return height - yScale(d);
