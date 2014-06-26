ad865c9695f23f03fac77d5497711529828a8dc8
Ray Villalobos <ray@lynda.com>
2014-06-25 23:15:09 -0400

    04_04 Ammended deletion of background

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var height = 400,
     barWidth = 50,
     barOffset = 5;
 
+var tempColor;
+
 var colors = d3.scale.linear()
 .domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
 .range(['#B58929','#C61C6F', '#268BD2', '#85992C'])
@@ -24,7 +26,6 @@ var xScale = d3.scale.ordinal()
 d3.select('#chart').append('svg')
     .attr('width', width)
     .attr('height', height)
-    .style('background', '#C9D7D6')
     .selectAll('rect').data(bardata)
     .enter().append('rect')
         .style('fill', function(d,i) {
@@ -40,3 +41,16 @@ d3.select('#chart').append('svg')
         .attr('y', function(d) {
             return height - yScale(d);
         })
+
+    .on('mouseover', function(d) {
+        tempColor = this.style.fill;
+        d3.select(this)
+            .style('opacity', .5)
+            .style('fill', 'yellow')
+    })
+
+    .on('mouseout', function(d) {
+        d3.select(this)
+            .style('opacity', 1)
+            .style('fill', tempColor)
+    })
